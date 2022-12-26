0106ffc604ab6850151767d88dbb1808760764d3
Avirup Basu <ami.avirup17@gmail.com>
2022-12-26 19:36:22 +0530

    Updated

--- python-serial/read.py
+++ python-serial/read.py
@@ -2,8 +2,7 @@ import serial
 
 ser = serial.Serial(port='/dev/cu.usbserial-0001',baudrate=9600)
 
-while(1):
-    x=ser.readline()
-    print(str(x,'UTF-8'))
-
-
+while True:
+    value= ser.readline()
+    valueInString=str(value,'UTF-8')
+    print(valueInString)
