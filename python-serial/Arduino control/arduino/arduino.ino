ebbb78e59e7cf391a7411c056816726db50b27ed
Avirup Basu <ami.avirup17@gmail.com>
2023-01-02 09:42:55 +0530

    Arduino files added

---
+++ python-serial/Arduino control/arduino/arduino.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (Serial.available() > 0)
+  {
+      char c= Serial.read();
+      if(c=='1')
+      {
+        digitalWrite(13,HIGH);
+      }
+      else if(c=='2')
+      {
+        digitalWrite(13,LOW);
+      }
+  }
+
+}
